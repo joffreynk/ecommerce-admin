76b811802589e7a5128e110b0e93fbb729257366
Joffreynk <joyjoffrey@gmail.com>
2023-07-22 01:49:11 +0200

    add toat provider

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -7,6 +7,7 @@ import * as z from "zod";
 import { useStoreModal } from "@/hooks/useStoreModal";
 import { Modal } from "@/components/ui/madal";
 import { log } from "console";
+import { useState } from "react";
 
 const formSchema = z
   .object({
@@ -16,6 +17,7 @@ const formSchema = z
 
 const StoreModal = () => {
   const storeModal = useStoreModal();
+  const [loading, setLoading] = useState(false)
 
   const {
     register,
@@ -29,9 +31,24 @@ const StoreModal = () => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log("====================================");
-    console.log(values);
-    console.log("====================================");
+    try {
+      setLoading(true)
+      const response = await fetch('/api/stores', {
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {
+          "content-type": "application/json",
+        }
+      })
+
+      const data = await response.json()
+      
+    } catch (error) {
+      console.log(error);
+      
+    }finally {
+      setLoading(false)
+    }
   };
 
   return (
@@ -47,6 +64,7 @@ const StoreModal = () => {
       >
         <div className="w-full flex flex-col gap-1">
           <input
+            disabled={loading}
             {...register("name")}
             placeholder="E-commerce"
             className="border p-2 text-lg rounded-md outline-none w-full "
@@ -59,12 +77,14 @@ const StoreModal = () => {
         </div>
         <div className="flex justify-end items-center gap-5 text-lg">
           <button
+             disabled={loading}
             className="bg-yellow-900 bg-opacity-40 hover:bg-yellow-600 rounded-xl px-4 py-2 text-white"
             onClick={storeModal.onClose}
           >
             cancel
           </button>
           <button
+             disabled={loading}
             className="bg-slate-600 rounded-xl px-4 py-2 hover:bg-green-900 text-white"
             type="submit"
           >
