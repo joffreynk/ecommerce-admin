9ba9da22409d59cb2a5e5daceacdae9e1450e0ee
Joffreynk <joyjoffrey@gmail.com>
2023-07-22 01:14:07 +0200

    add create store method

---
+++ app/api/stores/route.ts
@@ -0,0 +1,33 @@
+import prismadb from "@/utils/prismadb"
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+
+export const POST = async(req: Request,) => {
+  try {
+    const {userId} = auth()
+    const {name} = await req.json()
+
+    if(!userId) {
+      return new NextResponse("Unauthorize request", {status: 401})
+    }
+
+    if(!name) {
+      return new NextResponse("store name must be provided", {status: 400})
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId
+      }
+    })
+
+    return new NextResponse(JSON.stringify(store), {status: 201})
+
+    
+
+  } catch (error) {
+    
+  }
+}
