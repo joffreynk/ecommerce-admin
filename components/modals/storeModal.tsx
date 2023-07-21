f50c70189cde6db4a5682a819902805657103823
Joffreynk <joyjoffrey@gmail.com>
2023-07-21 21:42:51 +0200

    add form

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -1,21 +1,55 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
 import { useStoreModal } from "@/hooks/useStoreModal";
 import { Modal } from "@/components/ui/madal";
+import { log } from "console";
 
-import {zodResolver} from '@hookform/resolvers/zod'
+const formSchema = z.object({
+  name: z.string().min(1),
+}).required();
 
 const StoreModal = () => {
   const storeModal = useStoreModal();
-  return (<Modal
-    title='create store'
-    description="Add a new store to manage products and categories"
-    isOpen={storeModal.isOpen}
-    onClose={storeModal.onClose}
+
+  const { register, handleSubmit } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log("====================================");
+    console.log(values);
+    console.log("====================================");
+  };
+
+  return (
+    <Modal
+      title="create store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
     >
-      Future store Form
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col px-2 gap-6 py-4" >
+          <input
+            {...register("name")}
+            placeholder="E-commerce"
+            className="border p-2 text-lg rounded-md outline-none "
+          />
+          <div className="flex justify-end items-center gap-5 text-xl">
+            <button  >cancel</button>
+            <button type="submit">continue</button>
+          </div>
+        </form>
+      </div>
     </Modal>
-    )
+  );
 };
 
 export default StoreModal;
