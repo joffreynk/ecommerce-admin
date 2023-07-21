0a4ee590a6240cc19f9885a1831793f2a4617bbd
Joffreynk <joyjoffrey@gmail.com>
2023-07-21 20:21:10 +0200

    create reusable nodal component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,17 @@
 'use client'
 
+import { useStoreModal } from "@/hooks/useStoreModal"
+import { useEffect } from "react"
+
 export default function Home() {
+  const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [isOpen , onOpen])
+  
   return (
     <main >
       <h1>Hello admin</h1>
--- components/ui/madal.tsx
+++ components/ui/madal.tsx
@@ -1,94 +1,82 @@
-'use client'
+"use client";
 
-import { Dialog, Transition } from '@headlessui/react'
-import React, { Fragment, useState } from 'react'
+import { Dialog, Transition } from "@headlessui/react";
+import React, { Fragment, useState } from "react";
 
 interface ModalProps {
   title: string;
   description: string;
   isOpen: boolean;
   onClose: () => void;
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
-
-export const Modal: React.FC<ModalProps> = ({isOpen, description, title, onClose, children}) =>{
-
-  const onChange = () => {
-    if(!isOpen) {
+export const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  description,
+  title,
+  onClose,
+  children,
+}) => {
+  const close = () => {
+    if (isOpen) {
       onClose();
     }
-  }
+  };
 
   return (
-    <>
-      <div className="fixed inset-0 flex items-center justify-center">
-        <button
-          type="button"
-          onClick={onChange}
-          className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={close}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          {title}
-        </button>
-      </div>
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={onChange}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="div"
-                    className=" flex justify-between items-center"
-                  >
-                    <h4 className='text-2xl font-bold leading-6 text-gray-900' >
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="div"
+                  className=" flex justify-between items-center"
+                >
+                  <h4 className="text-2xl font-bold leading-6 text-gray-900">
                     {title}
-                    </h4>
-                    <div className="-mt-4">
+                  </h4>
+                  <div className="-mt-4">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={onChange}
+                      className="inline-flex justify-center text-2xl rounded-md border border-transparent bg-blue-100 px-3  font-medium text-blue-950 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={close}
                     >
                       x
                     </button>
                   </div>
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
-                  </div>
-
-                  
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">{description}</p>
+                  {children}
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
-        </Dialog>
-      </Transition>
-    </>
-  )
-}
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
