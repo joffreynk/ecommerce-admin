70a9ff9de5ce54effb78db642c2b81a9f5efdfae
Joffreynk <joyjoffrey@gmail.com>
2023-07-24 17:49:47 +0200

    add billboard page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Billboards() {
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Billboards;
