59c0072e1a2cd668deed9ff0fff21ce95505540d
Joffreynk <joyjoffrey@gmail.com>
2023-07-24 18:23:51 +0200

    add manage billboard inteface

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboard]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BillBoard = () => {
+  return (
+    <div>
+      BillBoard
+    </div>
+  )
+}
+
+export default BillBoard
--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,44 @@
 "use client";
 
 import Heading from "@/components/ui/Heading";
+import { useParams, useRouter } from "next/navigation";
 
 const BillBoardClient = () => {
+  const router = useRouter();
+  const params = useParams();
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title="BillBoard (0)" description="Maanage your billboards" />
+        <Heading
+          title="Bill Boards (0)"
+          description="Maanage your billboards"
+        />
+        <button
+          type="button"
+          //   disabled={loading}
+            onClick={() =>router.push(`/${params.storeId}/billboards/new`)}
+          className="bg-slate-700 p-2 rounded-md flex items-center justify-center text-white pr-4 "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v12m6-6H6"
+            />
+          </svg>
+          <span>Add new</span>
+        </button>
       </div>
+
+      <div className="h-[2px] my-2 w-full bg-white" />
+
     </>
   );
 };
