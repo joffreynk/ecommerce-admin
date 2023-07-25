918ad8e796e3e2cb33c524e896eecdc68461d972
Joffreynk <joyjoffrey@gmail.com>
2023-07-25 21:49:49 +0200

    try to fix problem

--- components/ui/BillBoardPageForm.tsx
+++ components/ui/BillBoardPageForm.tsx
@@ -11,6 +11,7 @@ import { toast } from "react-hot-toast";
 import AlertModal from "../modals/AlertModal";
 import ClipBoard from "@/components/ui/ClipBoard";
 import useOrigin from "@/utils/use-origin";
+import ImageUpload from "./imageUpload";
 
 const formSchema = z.object({
   label: z.string().min(2),
@@ -19,45 +20,62 @@ const formSchema = z.object({
 
 type BillBoardFormValues = z.infer<typeof formSchema>;
 
-const BillBoardPageForm = ({ initialData }: { initialData: billboard | null}) => {
+const BillBoardPageForm = ({
+  initialData,
+}: {
+  initialData: billboard | null;
+}) => {
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const router = useRouter();
   const params = useParams();
-  const origin = useOrigin()
+  const origin = useOrigin();
 
-
-  const title = initialData? "Edit bill Board": "crate bill Board";
-  const description = initialData? "Edit your existing bill Board": "create new bill Board for your store";
-  const toastmessageSuccess = initialData? "You have successfully Edited your bill Board": "You have successfully created a new bill Board";
-  const toastmessageError = initialData? "Failed to edit your bill board": "Failed to create new bill board";
-  const action = initialData? "Save changes": "Create";
+  const title = initialData ? "Edit bill Board" : "crate bill Board";
+  const description = initialData
+    ? "Edit your existing bill Board"
+    : "create new bill Board for your store";
+  const toastmessageSuccess = initialData
+    ? "You have successfully Edited your bill Board"
+    : "You have successfully created a new bill Board";
+  const toastmessageError = initialData
+    ? "Failed to edit your bill board"
+    : "Failed to create new bill board";
+  const action = initialData ? "Save changes" : "Create";
 
   const {
     handleSubmit,
     register,
-    formState: { errors },
+    formState: { errors, touchedFields },
   } = useForm<BillBoardFormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData||{label: "", imgUrl: ""},
+    defaultValues: initialData || { label: "", imgUrl: "" },
   });
 
+
+  console.log('====================================');
+  console.log(touchedFields);
+  console.log('====================================');
+
   const onSubmit = async (data: BillBoardFormValues) => {
-    try {
-      setLoading(true);
-      const response = await fetch(`/api/stores/${params.storeId}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
-      const store = await response.json();
-      router.refresh();
-      toast.success(toastmessageSuccess);
-    } catch (error: any) {
-      toast.error(toastmessageError);
-    } finally {
-      setLoading(false);
-    }
+    console.log('====================================');
+    console.log("DATA",data);
+    console.log('====================================');
+    // try {
+    //   setLoading(true);
+    //   const response = await fetch(`/api/stores/${params.storeId}`, {
+    //     method: "PATCH",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(data),
+    //   });
+    //   const store = await response.json();
+    //   router.refresh();
+    //   toast.success(toastmessageSuccess);
+    // } catch (error: any) {
+    //   toast.error(toastmessageError);
+    // } finally {
+    //   setLoading(false);
+    // }
   };
 
   const onDelete = async () => {
@@ -80,39 +98,37 @@ const BillBoardPageForm = ({ initialData }: { initialData: billboard | null}) =>
 
   return (
     <>
-    <AlertModal
-    isOpen={open}
-    onClose={()=>setOpen(false)}
-    onConfirm={onDelete}
-    loading={loading}
-    />
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading title={title} description={description} />
-        {
-          initialData && (
-            <button
-          type="button"
-          disabled={loading}
-          onClick={() =>setOpen(true)}
-          className="bg-red-700 p-2 rounded-xl"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-white"
+        {initialData && (
+          <button
+            type="button"
+            disabled={loading}
+            onClick={() => setOpen(true)}
+            className="bg-red-700 p-2 rounded-xl"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-            />
-          </svg>
-        </button>
-          )
-        }
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+          </button>
+        )}
       </div>
       <div className="h-[2px] my-2 w-full bg-white" />
 
@@ -121,6 +137,30 @@ const BillBoardPageForm = ({ initialData }: { initialData: billboard | null}) =>
         className="flex flex-col px-2 gap-6 py-4"
       >
         <div className="flex flex-col gap-1">
+          <label htmlFor="backgroundImage">Backgound Image</label>
+          {/* <input
+            disabled={loading}
+            {...register("imgUrl")}
+            defaultValue={initialData?.imgUrl}
+            className="border p-2 text-lg rounded-md outline-none w-72 md:w-96 "
+          /> */}
+
+          <ImageUpload
+          value={initialData?.imgUrl? [initialData?.imgUrl] : []} 
+          disabled={loading}
+          // onChange={(url)={
+          //   {...register({imgUrl: url})}
+          // }}
+          />
+          {errors.label && (
+            <p className="text-orange-300 " role="alert">
+              Bill Board name must be at least 2 characters
+            </p>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-1">
+        <label htmlFor="backgroundImage">Label</label>
           <input
             disabled={loading}
             {...register("label")}
@@ -139,7 +179,7 @@ const BillBoardPageForm = ({ initialData }: { initialData: billboard | null}) =>
             className="bg-slate-600 rounded-xl px-4 py-2 hover:bg-green-900 text-white"
             type="submit"
           >
-           {action}
+            {action}
           </button>
         </div>
       </form>
--- components/ui/imageUpload.tsx
+++ components/ui/imageUpload.tsx
@@ -69,21 +69,22 @@ const ImageUpload = ({
             open();
           };
           return (
-            <button type="button" onClick={onClick} disabled={disabled}>
+            <button type="button" onClick={onClick} disabled={disabled} className="flex items-center bg-slate-400 text-white gap-4 p-2 rounded-lg">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth="1.5"
                 stroke="currentColor"
-                className="w-4 h-4 mr-2"
+                className="w-6 h-6"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
                 />
               </svg>
+
               <h3>Upload Image</h3>
             </button>
           );
