76a11b2e1175c3c7349e2a30d2e47be12c0e2d6f
Joffreynk <joyjoffrey@gmail.com>
2023-07-26 08:45:54 +0200

    create get billboards

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -1,49 +1,74 @@
-import prismadb from "@/utils/prismadb"
-import { auth } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
+import prismadb from "@/utils/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-
-export const POST = async(req: Request, {params}: {params: {storeId: string}}) => {
+export const POST = async (
+  req: Request,
+  { params }: { params: { storeId: string } }
+) => {
   try {
-    const {userId} = auth()
-    const {label, imgUrl} = await req.json()
+    const { userId } = auth();
+    const { label, imgUrl } = await req.json();
 
-    if(!userId) {
-      return new NextResponse("UnAuthenticated request", {status: 401})
+    if (!userId) {
+      return new NextResponse("UnAuthenticated request", { status: 401 });
     }
 
-    if(!label.length) {
-      return new NextResponse("Label name must be provided", {status: 400})
+    if (!label.length) {
+      return new NextResponse("Label name must be provided", { status: 400 });
     }
-    if(!imgUrl.length) {
-        return new NextResponse("Image URL must be provided", {status: 400})
-      }
+    if (!imgUrl.length) {
+      return new NextResponse("Image URL must be provided", { status: 400 });
+    }
 
-      const storeByUserId = await prismadb.store.findFirst({
-        where: {
-          userId,
-          id: params.storeId
-        }
-      })
+    if (!params.storeId) {
+      return new NextResponse("Store ID is requequired", { status: 400 });
+    }
 
-      if(!storeByUserId) {
-        return new NextResponse("UnAuthorized request", {status: 403})
-      }
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        userId,
+        id: params.storeId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("UnAuthorized request", { status: 403 });
+    }
 
     const billboard = await prismadb.billboard.create({
       data: {
         label,
         imgUrl,
-        storeId: params.storeId
-      }
-    })
+        storeId: params.storeId,
+      },
+    });
 
-
-
-    return new NextResponse(JSON.stringify(billboard), {status: 201})
-
+    return new NextResponse(JSON.stringify(billboard), { status: 201 });
   } catch (error: any) {
-    console.log('[CREATE BILLBOARDS ERROR]', error)
-    return new NextResponse(error.message, {status: 500})
+    console.log("[CREATE BILLBOARDS ERROR]", error);
+    return new NextResponse(error.message, { status: 500 });
   }
-}
+};
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { storeId: string } }
+) => {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Store ID is requequired", { status: 400 });
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify(billboards), { status: 201 });
+  } catch (error: any) {
+    console.log("[GET BILLBOARDS ERROR]", error);
+    return new NextResponse(error.message, { status: 500 });
+  }
+};
